7cb7d647a69e4189ad8ef510dcea2e9087827b29
DanielAlejandroAnana <danana@frba.utn.edu.ar>
2022-06-05 21:38:25 -0300

    Agrego archivos necesarios para el TP (Falta README.md)

---
+++ 07-FasesErrores/hello3.c
@@ -0,0 +1,11 @@
+int printf(const char * restrict s, ...);
+int main(void){ 
+    int i=42;
+    prontf("La respuesta es %d\n");
+
+    // const char * s;
+    // s   const char *
+    // *s const char
+    // *s = 'b'
+    // s = 12
+    // printf("hola")
